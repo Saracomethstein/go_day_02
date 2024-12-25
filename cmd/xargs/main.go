bef5463c9538131a61649acaa77144a7054b0749
Adel Sevastianov <102406080+Saracomethstein@users.noreply.github.com>
2024-12-25 16:13:35 +0300

    GD2-10 up: code refactoring

--- cmd/xargs/main.go
+++ cmd/xargs/main.go
@@ -1,53 +1,7 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-	"os/exec"
-	"strings"
-)
+import "go_day_02/pkg/xargs"
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Fprintln(os.Stderr, "Usage: <program> <command> [args...]")
-		os.Exit(1)
-	}
-
-	command := os.Args[1]
-	cmdArgs := os.Args[2:]
-
-	scanner := bufio.NewScanner(os.Stdin)
-	var execErrors []string
-
-	for scanner.Scan() {
-		arg := strings.TrimSpace(scanner.Text())
-		if arg == "" {
-			continue
-		}
-
-		if err := executeCommand(command, cmdArgs, arg); err != nil {
-			execErrors = append(execErrors, fmt.Sprintf("arg '%s': %v", arg, err))
-		}
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading from stdin: %v\n", err)
-		os.Exit(1)
-	}
-
-	if len(execErrors) > 0 {
-		fmt.Fprintln(os.Stderr, "Errors occurred:")
-		for _, err := range execErrors {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		os.Exit(1)
-	}
-}
-
-func executeCommand(command string, cmdArgs []string, arg string) error {
-	cmd := exec.Command(command, append(cmdArgs, arg)...)
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	return cmd.Run()
+	xargs.Init()
 }
--- pkg/xargs/xargs.go
+++ pkg/xargs/xargs.go
@@ -1,3 +1,53 @@
 package xargs
 
-func Init() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+func Init() {
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "Usage: <program> <command> [args...]")
+		os.Exit(1)
+	}
+
+	command := os.Args[1]
+	cmdArgs := os.Args[2:]
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var execErrors []string
+
+	for scanner.Scan() {
+		arg := strings.TrimSpace(scanner.Text())
+		if arg == "" {
+			continue
+		}
+
+		if err := executeCommand(command, cmdArgs, arg); err != nil {
+			execErrors = append(execErrors, fmt.Sprintf("arg '%s': %v", arg, err))
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading from stdin: %v\n", err)
+		os.Exit(1)
+	}
+
+	if len(execErrors) > 0 {
+		fmt.Fprintln(os.Stderr, "Errors occurred:")
+		for _, err := range execErrors {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		os.Exit(1)
+	}
+}
+
+func executeCommand(command string, cmdArgs []string, arg string) error {
+	cmd := exec.Command(command, append(cmdArgs, arg)...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
